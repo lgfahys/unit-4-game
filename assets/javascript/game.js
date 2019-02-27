872a0f6fd719f5e387e110b114f1ec09a7bf8ae3
lgfahys <lindsey@codeasite.com>
2019-02-27 12:54:07 -0500

    got click functions for characters working and hid buttons until certain conditions are met

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,31 @@
 $(document).ready(function () {
+    // creating variables
+    var isCharacterChosen = false;
+    var isEnemyChosen = false;
+
+    //hiding buttons
+    $( "#attack" ).hide();
+    $( "#restart" ).hide();
 
     // array of characters
     var characters = [
-        {id: "darthVader", name: "Darth Vader", healthPoints: "150", attack: "25", counterAttack: "15", img: "assets/images/darthvader.jpg"},
-        {id: "lukeSkywalker", name: "Luke Skywalker", healthPoints: "120", attack: "10", counterAttack: "5", img: "assets/images/darthvader.jpg"}, 
-        {id: "princessLeia", name: "Princess Leia", healthPoints: "100", attack: "8", counterAttack: "20", img: "assets/images/darthvader.jpg"}, 
-        {id: "chewbacca", name: "Chewbacca", healthPoints: "100", attack: "20", counterAttack: "10", img: "assets/images/darthvader.jpg"}, 
+        { id: "darthVader", name: "Darth Vader", healthPoints: "150", attack: "25", counterAttack: "15", img: "assets/images/darthvader.jpg" },
+        { id: "lukeSkywalker", name: "Luke Skywalker", healthPoints: "120", attack: "10", counterAttack: "5", img: "assets/images/darthvader.jpg" },
+        { id: "princessLeia", name: "Princess Leia", healthPoints: "100", attack: "8", counterAttack: "20", img: "assets/images/darthvader.jpg" },
+        { id: "chewbacca", name: "Chewbacca", healthPoints: "100", attack: "20", counterAttack: "10", img: "assets/images/darthvader.jpg" },
     ];
- 
+
     // creating all character cards and appending to character div
-    for(i = 0; i < characters.length; i++) {
-        html="<div class='col'>";
-        html+="<div class='card character' id='"+characters[i].id+"'>";
-        html+="<img class='card-img' src='"+characters[i].img+"' alt='Card image'>";
-        html+="<div class='card-img-overlay'>";
-        html+="<h5 class='card-title'>"+characters[i].name+"</h5>";
-        html+="<p class='card-text healthpoints'>"+characters[i].healthPoints+"</p>";
-        html+="</div>";
-        html+="</div>";
-        html+="</div>";
+    for (i = 0; i < characters.length; i++) {
+        html = "<div class='col'>";
+        html += "<div class='card character' id='" + characters[i].id + "'>";
+        html += "<img class='card-img' src='" + characters[i].img + "' alt='Card image'>";
+        html += "<div class='card-img-overlay'>";
+        html += "<h5 class='card-title'>" + characters[i].name + "</h5>";
+        html += "<p class='card-text healthpoints'>" + characters[i].healthPoints + "</p>";
+        html += "</div>";
+        html += "</div>";
+        html += "</div>";
         $("#characters").append(html);
     }
 
@@ -27,10 +34,31 @@ $(document).ready(function () {
     // });
 
     // when user selects their character
-    $(".character").click(function () {
-        $(this).appendTo(".yourcharacter");
-        $("#characters").appendTo(".enemies");
-    });
+
+        $(".character").click(function () {
+            if (isCharacterChosen == false) { 
+                $(this).appendTo("#yourcharacter");
+                var chosenCharacter = $(this);
+                $("#characters").appendTo("#enemies");
+                isCharacterChosen = true;
+
+            // when user selects their enemy
+
+            } else {
+                $(this).appendTo("#fightsection");
+                $("#attack").show();
+                isEnemyChosen = true;
+            }
+
+        });
+
+    
+
+
+
+   
+
+
 
     // when user selects their enemy
 
